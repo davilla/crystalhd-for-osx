9c077d625f8611b6aa9ff2f9c318b8b815b33e7c
sdavilla <sdavilla@b7e64a4e-ef30-11de-8eb4-853c26953258>
2010-07-05 03:09:31 +0000

    change version_lnx.h to libcrystalhd_version.h to match upstream

--- crystalhd/linux_lib/libcrystalhd/libcrystalhd_if.cpp
+++ crystalhd/linux_lib/libcrystalhd/libcrystalhd_if.cpp
@@ -33,7 +33,7 @@
 #include <sys/ioctl.h>
 #include <unistd.h>
 #include "7411d.h"
-#include "version_lnx.h"
+#include "libcrystalhd_version.h"
 #include "bc_decoder_regs.h"
 #include "libcrystalhd_if.h"
 #include "libcrystalhd_priv.h"
